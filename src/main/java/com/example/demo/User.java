92ad845563b645a079f98daf723961761704bd7f
pro <techprosguide@gmail.com>
2025-06-18 21:30:20 +0300

    init commit

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
+@Table(name = "app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+    private String name;
+    private String email;
+}
